9943e864fac52a241050addc36a90e03922469ce
AyeEiPhyu <ayeeiphyu258@gmail.com>
2018-06-26 12:53:33 +0630

    ex101.py

---
+++ ex101.py
@@ -0,0 +1,15 @@
+tabby_dog="\tI'm tabbed in";
+persian_dog="I'm split\non s line."
+backslash_dog="i'm \\ a \\ dog"
+
+fat_cat = """
+I'll do a list:
+    \t*Dog=food
+    \t*Fishes
+    \t*Cantnip\n\t*Grass
+
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog))
+
