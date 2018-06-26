b0e6de2590509d23eef5b7998b0f27ebf57a0d19
AyeEiPhyu <ayeeiphyu258@gmail.com>
2018-06-26 13:01:35 +0630

    ex101.py

--- ex101.py
+++ ex101.py
@@ -1,15 +1,15 @@
-tabby_dog="\tI'm tabbed in";
+tabby_dog="\t I'm tabbed in"
 persian_dog="I'm split\non s line."
-backslash_dog="i'm \\ a \\ dog"
+backslash_dog="I'm \\ a \\ dog."
 
-fat_cat = """
+fat_dog = """
 I'll do a list:
-    \t*Dog=food
-    \t*Fishes
-    \t*Cantnip\n\t*Grass
-
+    \t=Dog food
+    \t=Fishes
+    \t=Cantnip\n\t*Grass
+"""
 print(tabby_dog)
 print(persian_dog)
 print(backslash_dog)
-print(fat_dog))
+print(fat_dog)
 
